b11e220bd35a3ab1956b149c8c761167904decb8
Daana Banerjee <dbanerjee889@gmail.com>
2016-07-24 13:30:35 -0700

    alcohol

--- josh.py
+++ josh.py
@@ -1,3 +1,17 @@
-num1 = 5
-num2 = 10
-print (num1+num2)
+"""
+hello
+string
+comment
+"""
+
+#num1 = 5
+#num2 = 10
+#print (num1+num2)
+name1 = raw_input('what is your name?')
+age1 = int(raw_input('how old are you %s?' % name1))
+if age1>=21:
+	print "you can drink wine!"
+elif age1 <0:
+	print "invalid"
+else:
+	print "LOSER"	
